04a8c06ebbf3cdbf7ed891e2fa2720d01bb65230
Flufflul <mufflul@gmail.com>
2021-01-28 20:12:56 -0500

    Added WebGL Build

--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -8,5 +8,8 @@ public class Rotator : MonoBehaviour
     void Update()
     {
         transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+        // transform.Rotate(new Vector3(0, 30, 0) * Time.deltaTime);
+        // transform.Rotate(new Vector3(0, 0, 45) * Time.deltaTime);
+        // transform.Rotate(new Vector3(15, 0, 0) * Time.deltaTime);
     }
 }
